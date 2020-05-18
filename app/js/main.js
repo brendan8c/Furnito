1924cd066543bc089cad37476e9b8ffe70103733
brendan8c <brendan8c@gmail.com>
2020-05-18 05:43:09 +0300

    start of a new project

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+// #1grn This video player is connected. But I use another
+// $(function() {
+//     $(document).ready(function() {
+//         var videobackground = new $.backgroundVideo($('.header__video1'), {
+//             "align": "centerXY",
+//             "width": 1280,
+//             "height": 720,
+//             "path": "media/",
+//             "filename": "moves_01",
+//             "types": ["mp4", "ogg", "webm"],
+//             "preload": true,
+//             "autoplay": true,
+//             "loop": true
+//         });
+//     });
+// });
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
+
+
+gulp.task('style', function() {
+    return gulp.src([
+            'node_modules/normalize.css/normalize.css',
+        ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('script', function() {
+    return gulp.src([
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/Vide-0.5.1/dist/jquery.vide.js',
+            'node_modules/HTML5-Background-Video/jquery.backgroundvideo.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
+});
+
+gulp.task('script-min', function() {
+    return gulp.src([
+            'app/js/main.js',
+        ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 8 versions'] }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
+});
+gulp.task('js', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('default', gulp.parallel('script', 'style', 'script-min', 'sass', 'watch', 'browser-sync'))
